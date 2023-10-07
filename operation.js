a6359ec44a9709a87142bfef6d8a24bfa1aa9f31
Jerinislam05 <jerinislam05@gmail.com>
2023-10-08 00:36:54 +0100

    operation file

---
+++ operation.js
@@ -0,0 +1,22 @@
+var onionPrice = 20;
+var eggPrice = 15;
+
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+// multiplication 
+var quantity = 10;
+var totalCost = eggPrice * quantity;
+// console.log(totalCost);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayer = money/player;
+// console.log(eachPlayer);
