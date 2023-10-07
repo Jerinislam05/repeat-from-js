69209a6049e488e76a96f5ce4b6da1a95ff9f4dc
Jerinislam05 <jerinislam05@gmail.com>
2023-10-08 00:05:30 +0100

    learning variables

---
+++ variable-name.js
@@ -0,0 +1,18 @@
+//1. variable can not be any keywords
+var false = 20;
+var return = true;
+
+//2. variable name has to be in one word & no space
+var my home adress = "UK"
+
+//3. variable name can not have quotation
+var 'name' = 'jerin islam';
+
+//4. variable name can not start with a number
+var 14club = 1992;
+
+//5. can not use dash between variable name, use underscore/camelcase
+var user-name = 'jerin';
+var user_name = 'marston avenue';
+
+//6. variable name is case sensitive
---
+++ variable.js
@@ -0,0 +1,17 @@
+// vary + able = variable
+
+//numeric (5ta jinish thakbe kintu man gulo number e hobe)
+var age = 28;
+var value = 38;
+var temp = 17;
+
+// string (borno diye man hoy, r man gulo quotation er moddhe thakbe)
+var person = 'jerin';
+var location = 'london';
+var special = 'tareq';
+
+// boolean (borno holeo key word diye prokash kore ex:true/false, tai quotation lagena)
+var serious = true;
+var single = false;
+var marks = true
+
