95f547d77db11b53cc9a4e6b8d6a89b480324693
Jerinislam05 <jerinislam05@gmail.com>
2023-10-08 12:48:47 +0100

    learn how to convert string to a number

---
+++ conversion.js
@@ -0,0 +1,13 @@
+var price1 = '40';
+var price2 = '20';
+var totalPrice = price1 + price2;
+console.log(totalPrice); //string er moddhe jodi number thake tateo pashapashi boshbe, jog hobena
+
+// convert string to number
+console.log(price1);
+var price1Num = parseInt(price1);//kono string er moddhe jodi number thakle setake jodi number e convert korte hoy tahole parseInt use korte hoy
+console.log(price1Num);
+
+var gpa = '3.43';
+var gpaNum = parseFloat(gpa);//string er moddhe doshomik wala number er jonne parseFloat use korte hoy
+console.log(gpaNum);
