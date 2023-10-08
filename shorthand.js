879e70ee9a1631f2a834c3d0b8c36a1682589c37
Jerinislam05 <jerinislam05@gmail.com>
2023-10-08 11:59:19 +0100

    add shorhand system

---
+++ shorthand.js
@@ -0,0 +1,40 @@
+var tomatoPrice = 25;
+tomatoPrice = 30; //variable update korte/man change korte var diya lagena
+
+// add korbo ager man er sthe
+var price1 = 31;
+var price2 = 10;
+
+// price1 = 35; //add new value
+
+// ager man er sthe add korbo
+// price1 = price1 + 10;
+// console.log(price1);
+
+// addition to the same variable shorthand
+// += means adding some existing value
+price1 += 10;
+// console.log(price1);
+
+// -= means subtract some existing value
+price2 -= 2;
+// console.log(price2);
+
+// *= means multiply
+price1 *= 5;
+// console.log(price1);
+
+// /= means divide
+price2 /= 2;
+// console.log(price2);
+
+// ++ means adding 1 & -- means subtracting 1
+var age = 15;
+// age = age + 1; //age++;
+age++;
+// console.log(age);
+
+var age2 = 30;
+// age2 = age2 - 1; // age--;
+age2--;
+// console.log(age2);
