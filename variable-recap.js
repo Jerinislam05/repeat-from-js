a67be48868f565cf88bba182342431dd2a952a83
Jerinislam05 <jerinislam05@gmail.com>
2023-10-09 22:09:59 +0100

    recap the whole module

---
+++ variable-recap.js
@@ -0,0 +1,28 @@
+// number type variable
+var sunglass = 100;
+
+// string type variable
+var specialOne = "jerin"
+
+// boolean type variable
+var isHappy = true;
+
+// to types of number: Integer: 120,
+// float, decimal: 3.43
+
+// variable naming convention: rules, best practice
+/*
+1. single word
+2. no gap or space
+3. no keyword
+4. should use camel case
+5. no dash
+6. can not start with number
+*/
+
+// math operations: +, -, *, /,
+// shorthand: +=, -=, ++, --
+
+// convert: parseint, parsefloat
+// toFixed
+// reminder : vagsesh, vagfol
