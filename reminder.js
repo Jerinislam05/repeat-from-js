1e475d577508359bc68c0f73f92408097120e5f4
Jerinislam05 <jerinislam05@gmail.com>
2023-10-09 21:52:37 +0100

    add types and reminder file

---
+++ reminder.js
@@ -0,0 +1,7 @@
+var mangoes = 7;
+var eater = 2;
+
+// (/) means vagfol
+// (%) means vagsesh
+console.log(mangoes/eater);
+console.log(mangoes%eater);
---
+++ types.js
@@ -0,0 +1,14 @@
+// typeof diye sob variable er type ber korte hoy
+var price = 100;
+console.log(typeof price);
+var price1 = "100";
+console.log(typeof price1);
+var isHappy = true;
+console.log(typeof isHappy);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+console.log(sum);
